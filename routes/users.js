5911d723209c82605f99ad61bce4bc5e54d10623
Rob Taylor <robert.i.taylor88@gmail.com>
2020-09-04 17:10:48 +0100

    change user lookup to me lookup

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const User = require("../models/user.model");
+const { jwtCheck } = require("../services/jwt-check.service");
 const router = express.Router();
 const debug = require("debug")("http");
+const crypto = require("crypto");
+
+function sha256(buffer) {
+  return crypto.createHash("sha256").update(buffer, "utf8").digest("hex");
+}
+
+router.get("/me", jwtCheck, (req, res) => {
+  const userId = sha256(req.user.sub);
+  User.findOne({ userId })
+    .then((user) => {
+      res.status(200).send(user);
+    })
+    .catch((err) => res.status(400).send(err));
+});
 
 router.get("/:userId", (req, res) => {
   const { userId } = req.params;
